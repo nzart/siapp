5ebdc2a1a47505dde2fd28d38d6ae54001d9dbdd
Nick Zart <nzart@g.clemson.edu>
2019-03-26 13:27:57 -0400

    Init

---
+++ app.js
@@ -0,0 +1,102 @@
+//Get Data
+
+//Empty array for storing
+var added = [];
+
+//Get Data
+/*var userInput = function() {
+    return {
+        //name: document.getElementById('name').value,
+        amount: document.getElementById('amount').value
+    }
+};*/
+
+var userInput = function(){
+    return parseFloat(document.getElementById('amount').value);
+}
+
+// Store Data
+/*var newSugar = function(){
+    return added.push(userInput());
+}*/
+
+var newSugar = function(){
+    return added.push(userInput());
+}
+
+// New HTML
+function newBox() {
+    var newLi = document.createElement('li');
+    newLi.classList.add('twogrid');
+    
+    var newName = document.getElementById('name').value;    
+    var newH = document.createElement('div');
+    newH.classList.add('work');
+    var n = document.createTextNode(newName);
+    newLi.appendChild(newH);
+    newH.appendChild(n);
+    
+    var newAmount = document.getElementById('amount').value;
+    var newA = document.createElement('div');
+    newA.classList.add('work');
+    var a = document.createTextNode(newAmount);
+    newLi.appendChild(newA);
+    newA.appendChild(a);
+    
+    var boxhold = document.getElementById('boxhold').getElementsByTagName('ul')[0];
+    
+    boxhold.appendChild(newLi);    
+}
+
+//Add total
+function total() {
+    var sum = 0;    
+    for (var i = 0; i < added.length; i++) {
+        sum += added[i];
+    }
+    //return yeet;
+    console.log(added);
+    
+    document.getElementById('total').textContent = sum;
+};
+
+//Clear after submit
+function clear() {
+    //document.getElementsByTagName('input').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('amount').value = '';
+}
+
+//Adding stuff
+
+var displayData = (function() {
+    
+    var addInput = function() {
+        var data = userInput();
+        //var number = num();
+        var item = newSugar();
+        var box = newBox();
+        var tot = total();
+        var c = clear();
+    };
+  
+    var addFood = document.getElementById('addFood');
+    addFood.addEventListener('click', addInput);
+    
+    document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                addInput();
+            }
+        });    
+    
+})(userInput, newSugar, newBox, total, clear);
+
+
+
+
+
+
+
+
+
+
